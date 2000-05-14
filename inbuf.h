b05189c2f90c59056d163c8c0109658baaf50829
Dieter Baron <dillo@nih.at>
2000-05-14 12:38:19 +0000

    added copyright messages

--- inbuf.h
+++ inbuf.h
@@ -1,6 +1,30 @@
 #ifndef HAD_INBUF_H
 #define HAD_INBUF_H
 
+/*
+  inbuf.h -- random access to a stretch of a file
+  Copyright (C) 2000 Dieter Baron
+
+  This file is part of malint, an MPEG Audio stream validator.
+  The author can be contacted at <dillo@giga.or.at>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+
 #include <stdio.h>
 
 #define INBUF_MAX_KEEP	16
--- mpeg.h
+++ mpeg.h
@@ -1,6 +1,31 @@
 #ifndef HAD_MPEG_H
 #define HAD_MPEG_H
 
+/*
+  mpeg.h -- MPEG tables and field access macros
+
+  Copyright (C) 2000 Dieter Baron
+
+  This file is part of malint, an MPEG Audio stream validator.
+  The author can be contacted at <dillo@giga.or.at>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+
 extern int _mp3_samp_tab[2][4];		/* in build_frame_tab.c */
 extern int _mp3_bit_tab[2][16][3];	/* in build_frame_tab.c */
 extern int _mp3_jsb_tab[3][4];
